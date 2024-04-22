223c30371b9daee356ecc12beaca620455648593
Nahom Garefo <harobina28@gmail.com>
2024-04-22 15:11:09 +0300

    fixed some things i forgot in the previous commit

--- providers_lab/states_/lib/main.dart
+++ providers_lab/states_/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:states_/counter.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (context) => Counter(), child: const MyApp()));
+  runApp(
+      ChangeNotifierProvider(create: (context) => Counter(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,21 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,36 +24,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -84,29 +48,30 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: Stack(children: <Widget>[
-        Positioned(
-          bottom: 10.0,
-          right: 10.0,
-          child: FloatingActionButton(
-            onPressed: () => context.read<Counter>().decremnt(),
-            tooltip: 'Deceremnt',
-            child: const Icon(Icons.remove_circle),
-          ),
-        ),
-        Positioned(
-          bottom: 70.0,
-          right: 10.0,
-          child: FloatingActionButton(
-            onPressed: () => context.read<Counter>().increment(),
-            tooltip: 'Increment',
-            child: const Icon(
-              Icons.add,
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: FloatingActionButton(
+              onPressed: () => context.read<Counter>().decremnt(),
+              tooltip: 'Deceremnt',
+              child: const Icon(Icons.remove_circle),
             ),
           ),
-        )
-      ]),
-      // This trailing comma makes auto-formatting nicer for build methods.
+          Positioned(
+            bottom: 70.0,
+            right: 10.0,
+            child: FloatingActionButton(
+              onPressed: () => context.read<Counter>().increment(),
+              tooltip: 'Increment',
+              child: const Icon(
+                Icons.add,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
